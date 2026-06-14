55217a086dc9152ca3b3dfc96911be07f31b413f
GitHubUserShrek <171639939+GitHubUserShrek@users.noreply.github.com~>
2026-06-14 13:41:07 -0400

    fix audio settings save to on input instead of on menu exit

--- src/menu.cpp
+++ src/menu.cpp
@@ -275,17 +275,20 @@ static void act_vol_up()
 {
     audio.setVolume(std::min(255, (int)audio.getVolume() + 32));
     audio.beep(440, 20);
+    audio.saveSettings();        
 }
 static void act_vol_down()
 {
     audio.setVolume(std::max(0, (int)audio.getVolume() - 32));
     audio.beep(440, 20);
+    audio.saveSettings();       
 }
 static void act_mute()
 {
     audio.toggleMute();
     if (!audio.isMuted())
         audio.beep(440, 20);
+    audio.saveSettings();        
 }
 
 static void act_wifi_info()
